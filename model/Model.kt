0ebb37242185e587df80e7bb9e403661cba57dbc
ullmans <shilo.ullman@gmail.com>
2021-06-24 01:16:02 +0300

    added the thread and the queue to the model

--- model/Model.kt
+++ model/Model.kt
@@ -3,26 +3,64 @@ package model
 import java.io.PrintWriter
 import java.net.Socket
 
+//package com.bezkoder.kotlin.queue
+
+import java.util.Queue
+//import java.util.LinkedList
+import java.util.LinkedBlockingQueue
+
 class Model(ip: String?, port: Int) {
+
     private val out: PrintWriter
+    val funcs: Queue<String> = LinkedBlockingQueue<String>()
+    val values: Queue<Int> = LinkedBlockingQueue<Int>()
+
+    thread{
+        while(true){
+            if (!funcs.isEmpty()){
+
+                f = funcs.poll()
+                n = values.poll()
+
+                if(f == "setAileron"){
+                    out.print("set /controls/flight/aileron$`val`\r\n")
+                    out.flush()
+                } else if (f == "setElevator"){
+                    out.print("set /controls/flight/elevator$`val`\r\n")
+                    out.flush()
+                } else if(f == "setRudder"){
+                    out.print("set /controls/flight/rudder$`val`\r\n")
+                    out.flush()
+                } else if (f == "setThrottle"){
+                    out.print("set /controls/engines/current-engine/throttle$`val`\r\n")
+                    out.flush()
+                }
+            }
+        }
+    }
+
     fun setAileron(`val`: Int) {
-        out.print("set /controls/flight/aileron$`val`\r\n")
-        out.flush()
+        nums.add("setAileron")
+//        out.print("set /controls/flight/aileron$`val`\r\n")
+//        out.flush()
     }
 
     fun setElevator(`val`: Int) {
-        out.print("set /controls/flight/elevator$`val`\r\n")
-        out.flush()
+        nums.add("setElevator")
+//        out.print("set /controls/flight/elevator$`val`\r\n")
+//        out.flush()
     }
 
     fun setRudder(`val`: Int) {
-        out.print("set /controls/flight/rudder$`val`\r\n")
-        out.flush()
+        nums.add("setRudder")
+//        out.print("set /controls/flight/rudder$`val`\r\n")
+//        out.flush()
     }
 
     fun setThrottle(`val`: Int) {
-        out.print("set /controls/engines/current-engine/throttle$`val`\r\n")
-        out.flush()
+        nums.add("setThrottle")
+//        out.print("set /controls/engines/current-engine/throttle$`val`\r\n")
+//        out.flush()
     }
 
     init {
